c16a777256685565e4c5c5357c93d323d8a35d3a
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2025-06-07 10:50:40 +0200

    feature: Set up initial folder organization (#1)

---
+++ BRSilvia/BRSilvia/Code/App/BRSilviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BRSilviaApp.swift
+//  BRSilvia
+//
+//  Created by Silvia España Gil on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BRSilviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
