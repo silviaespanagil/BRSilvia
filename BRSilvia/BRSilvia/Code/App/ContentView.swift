3e5b2b7b4eb7793a7e6e1f42a9abf3135afd659b
Silvia España Gil <81619759+silviaespanagil@users.noreply.github.com>
2025-06-07 10:57:24 +0200

    features: Add users json to resources

--- BRSilvia/BRSilvia/Code/App/ContentView.swift
+++ BRSilvia/BRSilvia/Code/App/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("Test JSON") {
+                if let path = Bundle.main.path(forResource: "users", ofType: "json") {
+                    print("JSON found at: \(path)")
+                } else {
+                    print("JSON not found")
+                }
+            }
         }
         .padding()
     }
